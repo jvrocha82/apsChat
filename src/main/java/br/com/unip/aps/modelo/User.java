4bfcdb7d2db5be6f11dfce0173e88c167e7af8a6
Gustavo-SantosJ <83554496+Gustavo-SantosJ@users.noreply.github.com>
2021-05-17 02:57:12 -0300

    Versão 1.5

--- src/main/java/br/com/unip/aps/modelo/User.java
+++ src/main/java/br/com/unip/aps/modelo/User.java
@@ -2,8 +2,9 @@ package br.com.unip.aps.modelo;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class User{
